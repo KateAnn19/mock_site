6880e2f60a8a0059a590eb85fff742ad91496b47
kate mcgee <mcgeehee@gmail.com>
2020-09-17 18:24:57 -0600

    added files and first blog post

---
+++ mock/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import BlogContainer from "./BlogContainer";
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Teacher Turned Developer Chronicles</h1>
+        <h3><em>A day in the life of an aspiring developer</em></h3>
+        <BlogContainer/>
+
+    </div>
+  );
+}
+
+export default App;
