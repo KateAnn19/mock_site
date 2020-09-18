933f40933a5333b8a6711331479dcf760f0a588a
kate mcgee <mcgeehee@gmail.com>
2020-09-17 18:39:19 -0600

    added in articles

--- mock/src/BlogContainer.js
+++ mock/src/BlogContainer.js
@@ -7,26 +7,31 @@ function BlogContainer() {
       <div className="post">
         <div class="date">September 17, 2020</div>
         <h2>Prologue</h2>
-
         <p class="quote">
-          <span>🙂     </span>Before I decided to make a career change to software development, I
-          was a teacher. I worked with kids and adults with developmental
-          disabilities. I have worked in this field for over seven years. I
-          enjoyed the work I was doing, but I couldn’t see myself moving up into
-          other positions because I didn’t want to work in crisis management
-          every day. For some people, it’s possible, but, for me, it wasn’t.</p> <p>I
-          struggle with anxiety, and I knew these crises were only exacerbating
-          my anxiety more. I also knew I needed to make more money if I couldn’t
-          pursue upward management and do something I enjoyed and that I could
-          do for the long-term.</p>
-
+          <span>🙂 </span>Before I decided to make a career change to software
+          development, I was a teacher. I worked with kids and adults with
+          developmental disabilities. I have worked in this field for over seven
+          years. I enjoyed the work I was doing, but I couldn’t see myself
+          moving up into other positions because I didn’t want to work in crisis
+          management every day. For some people, it’s possible, but, for me, it
+          wasn’t.
+        </p>{" "}
+        <p>
+          I struggle with anxiety, and I knew these crises were only
+          exacerbating my anxiety more. I also knew I needed to make more money
+          if I couldn’t pursue upward management and do something I enjoyed and
+          that I could do for the long-term.
+        </p>
         <p>
           Landing on web development was an unlikely choice. I hadn’t ever done
           coding before, and I thought that I had to be superhuman smart to do
           it. I also didn’t look or act the part. I wasn’t into video games. I
           didn’t stay up to date on the latest technology. I prided myself on
-          being a flip phone user for longer than everyone else (so hipster 😃 ).</p>
-          <p>Technology was vital to me because I used it, but I wasn’t obsessed.
+          being a flip phone user for longer than everyone else (so hipster 😃
+          ).
+        </p>
+        <p>
+          Technology was vital to me because I used it, but I wasn’t obsessed.
           So, to be honest, the only reason Web Development was even on the list
           was because of a friend who happens to be a software developer told me
           that I’d make a good developer. I was like, “me?.....Nah”. But he was
@@ -36,7 +41,6 @@ function BlogContainer() {
           realized that I had many misconceptions about web development,
           programming, and software development.
         </p>
-
         <p>
           I realized I don’t have to be superhuman smart, that the field is
           becoming more diverse and accessible and that it’s also not people in
@@ -47,55 +51,147 @@ function BlogContainer() {
           get it at first, but I kept going because I enjoyed it. I enjoyed it a
           lot more than I thought I would. I found myself wanting to understand
           everything and wanting to figure things out and couldn’t let things go
-          until I got them.</p><p>I leaned heavily on my fiance, who is a hardware
-          engineer. He helped me understand the basics and encouraged me to keep
-          going. I’m lucky to have him, and everyone needs a champion. But I
-          think I still would have pursued this regardless. Still, I am so glad
-          he has been there.</p> After completing my Javascript course, I got pretty
-          serious about looking into Bootcamps until I finally landed on Lambda
-          School. I wanted to choose the right one for me, and Lambda School
-          checked off all the boxes. They offer an ISA making it even more
-          possible for me to make this career transition. I enrolled and went
-          through the program. <p>I had ups and downs, but I kept persevering
-          because I wanted to. In the past, I persevered because I had to. This
-          time around, I persevered because I was enjoying what I was doing and
-          felt inspired. 💃  I also felt like I still had energy and emotion left
-          for my partner and family after the day ended, which is very different
-          from before. After work most days in my teaching job, I’d be utterly
-          exhausted and had nothing left. I’d even cry just out of exhaustion.
-          Now I stay up late because I just want that page to look good. It’s
-          fun, enjoyable and there’s so much room to grow and learn that I’ll
-          never get bored.
+          until I got them.
         </p>
+        <p>
+          I leaned heavily on my fiance, who is a hardware engineer. He helped
+          me understand the basics and encouraged me to keep going. I’m lucky to
+          have him, and everyone needs a champion. But I think I still would
+          have pursued this regardless. Still, I am so glad he has been there.
+        </p>{" "}
+        After completing my Javascript course, I got pretty serious about
+        looking into Bootcamps until I finally landed on Lambda School. I wanted
+        to choose the right one for me, and Lambda School checked off all the
+        boxes. They offer an ISA making it even more possible for me to make
+        this career transition. I enrolled and went through the program.{" "}
+        <p>
+          I had ups and downs, but I kept persevering because I wanted to. In
+          the past, I persevered because I had to. This time around, I
+          persevered because I was enjoying what I was doing and felt inspired.
+          💃 I also felt like I still had energy and emotion left for my partner
+          and family after the day ended, which is very different from before.
+          After work most days in my teaching job, I’d be utterly exhausted and
+          had nothing left. I’d even cry just out of exhaustion. Now I stay up
+          late because I just want that page to look good. It’s fun, enjoyable
+          and there’s so much room to grow and learn that I’ll never get bored.
+        </p>
+        <hr></hr>
       </div>
 
       <div class="post">
-        <div class="date">December 11 2015</div>
-        <h2>This Is Another Article</h2>
+        <div class="date">September 25, 2020</div>
+        <h2>The Job Hunt</h2>
 
         <p class="quote">
-          Bacon ipsum dolor amet capicola strip steak landjaeger, biltong spare
-          ribs rump cow ground round andouille sirloin pork. Short ribs pig
-          prosciutto swine. Flank turducken turkey rump, leberkas shoulder
-          bresaola ham hock tail drumstick corned beef. Venison pork chop beef
-          jowl short ribs.
+          I'm baby coloring book blue bottle gluten-free pinterest artisan,
+          photo booth gochujang raw denim roof party wayfarers mumblecore ugh
+          synth seitan. Subway tile kale chips paleo pug hammock church-key.
+          Master cleanse copper mug normcore taiyaki. Etsy normcore bitters
+          taiyaki craft beer four loko kale chips enamel pin. Tote bag vinyl
+          tousled, pug chicharrones twee disrupt. Celiac flannel deep v try-hard
+          green juice.
         </p>
 
         <p>
-          Shankle beef ribs tongue strip steak flank landjaeger capicola
-          hamburger chuck pancetta kevin. Sirloin landjaeger chicken, bresaola
-          brisket swine short ribs turkey short loin ball tip porchetta ham
-          hock. Capicola frankfurter jowl short loin. Kevin flank hamburger,
-          beef venison shankle short loin bresaola frankfurter
+          Chia quinoa raclette, snackwave kickstarter deep v yuccie
+          vexillologist. Cronut prism organic, microdosing heirloom shoreditch
+          +1 skateboard mlkshk. Kogi XOXO knausgaard occupy, polaroid hammock
+          cold-pressed enamel pin narwhal tattooed celiac. Gluten-free pok pok
+          cronut lo-fi. Keytar wolf swag everyday carry squid edison bulb, photo
+          booth try-hard.
         </p>
 
         <p>
-          Bresaola short ribs pastrami, beef ribs spare ribs kielbasa ham tongue
-          kevin landjaeger chicken ball tip. Pork chop beef kevin strip steak,
-          chicken pork belly pastrami ham hock flank shoulder chuck turkey
-          ribeye andouille ball tip. Leberkas ham ham hock pork loin. Filet
-          mignon bacon pancetta leberkas turducken fatback tongue frankfurter
-          jowl. Shoulder tenderloin chicken shank bacon shankle sirloin.
+          Cold-pressed chillwave brooklyn organic wayfarers swag. Tbh kombucha
+          activated charcoal iceland pickled irony kinfolk mlkshk sartorial
+          portland copper mug. La croix helvetica small batch chia squid
+          shoreditch authentic wolf woke dreamcatcher food truck flexitarian
+          normcore butcher. Sriracha +1 air plant occupy, kickstarter yuccie
+          franzen normcore. Church-key 3 wolf moon viral everyday carry truffaut
+          art party.
+        </p>
+        <hr></hr>
+      </div>
+    
+      <div class="post">
+        <div class="date">October 2, 2020</div>
+        <h2>The Job - Kirschbaum Hires Me </h2>
+
+        <p class="quote">
+          Yuccie beard chartreuse, fanny pack small batch venmo cornhole
+          disrupt. Air plant quinoa crucifix austin, beard ramps roof party yr
+          authentic 3 wolf moon viral. +1 blue bottle authentic woke raclette
+          activated charcoal seitan art party austin pok pok master cleanse food
+          truck fam. Woke thundercats art party lomo skateboard kale chips.
+          Copper mug seitan tousled shoreditch, cliche celiac biodiesel kogi
+          plaid. Umami small batch cornhole blue bottle before they sold out,
+          succulents wayfarers. Shabby chic poke affogato, snackwave typewriter
+          ramps intelligentsia small batch you probably haven't heard of them
+          meditation tumeric jean shorts man braid tbh green juice.
+        </p>
+
+        <p>
+          Chia quinoa raclette, snackwave kickstarter deep v yuccie
+          vexillologist. Cronut prism organic, microdosing heirloom shoreditch
+          +1 skateboard mlkshk. Kogi XOXO knausgaard occupy, polaroid hammock
+          cold-pressed enamel pin narwhal tattooed celiac. Gluten-free pok pok
+          cronut lo-fi. Keytar wolf swag everyday carry squid edison bulb, photo
+          booth try-hard.
+        </p>
+
+        <p>
+          Cold-pressed chillwave brooklyn organic wayfarers swag. Tbh kombucha
+          activated charcoal iceland pickled irony kinfolk mlkshk sartorial
+          portland copper mug. La croix helvetica small batch chia squid
+          shoreditch authentic wolf woke dreamcatcher food truck flexitarian
+          normcore butcher. Sriracha +1 air plant occupy, kickstarter yuccie
+          franzen normcore. Church-key 3 wolf moon viral everyday carry truffaut
+          art party.
+        </p>
+        <hr></hr>
+      </div>
+      <div class="post">
+        <div class="date">October 9, 2020</div>
+        <h2>Week 1 At Kirschbaum</h2>
+        <p className="note">
+          <em>
+            Please note that there will be a blog post with an update of
+            progress from here on out each week, including what I’ve learned,
+            struggles, successes, failures, and tech tips for other aspiring
+            developers.
+          </em>
+        </p>
+
+        <p class="quote">
+          Yuccie beard chartreuse, fanny pack small batch venmo cornhole
+          disrupt. Air plant quinoa crucifix austin, beard ramps roof party yr
+          authentic 3 wolf moon viral. +1 blue bottle authentic woke raclette
+          activated charcoal seitan art party austin pok pok master cleanse food
+          truck fam. Woke thundercats art party lomo skateboard kale chips.
+          Copper mug seitan tousled shoreditch, cliche celiac biodiesel kogi
+          plaid. Umami small batch cornhole blue bottle before they sold out,
+          succulents wayfarers. Shabby chic poke affogato, snackwave typewriter
+          ramps intelligentsia small batch you probably haven't heard of them
+          meditation tumeric jean shorts man braid tbh green juice.
+        </p>
+
+        <p>
+          Chia quinoa raclette, snackwave kickstarter deep v yuccie
+          vexillologist. Cronut prism organic, microdosing heirloom shoreditch
+          +1 skateboard mlkshk. Kogi XOXO knausgaard occupy, polaroid hammock
+          cold-pressed enamel pin narwhal tattooed celiac. Gluten-free pok pok
+          cronut lo-fi. Keytar wolf swag everyday carry squid edison bulb, photo
+          booth try-hard.
+        </p>
+
+        <p>
+          Cold-pressed chillwave brooklyn organic wayfarers swag. Tbh kombucha
+          activated charcoal iceland pickled irony kinfolk mlkshk sartorial
+          portland copper mug. La croix helvetica small batch chia squid
+          shoreditch authentic wolf woke dreamcatcher food truck flexitarian
+          normcore butcher. Sriracha +1 air plant occupy, kickstarter yuccie
+          franzen normcore. Church-key 3 wolf moon viral everyday carry truffaut
+          art party.
         </p>
       </div>
     </div>
