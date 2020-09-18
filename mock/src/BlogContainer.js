e58303fed9fd31a4a06c83b26c6b5164f3038a84
kate mcgee <mcgeehee@gmail.com>
2020-09-18 10:49:34 -0600

    changed paragraph

--- mock/src/BlogContainer.js
+++ mock/src/BlogContainer.js
@@ -9,86 +9,32 @@ function BlogContainer() {
           <div class="date">September 17, 2020</div>
           <h2>Prologue</h2>
           <p class="quote">
-            <span role="img" aria-label="smiley face">
-              🙂{" "}
-            </span>
-            Before I decided to make a career change to software development, I worked with kids 
-            and adults with developmental
-            disabilities. I have worked in this field for over seven years. I
-            enjoyed the work I was doing, but I couldn’t see myself moving up
-            into other positions because I didn’t want to work in crisis
-            management every day. For some people, it’s possible, but, for me,
-            it wasn’t.
-          </p>{" "}
-          <p>
-            I struggle with anxiety, and I knew these crises were only
-            exacerbating my anxiety more. I also knew I needed to make more
-            money if I wasn't going to pursue upward management or other upward moving positions. And I needed to do something I
-            enjoyed and that I could do for the long-term.
+            I'm baby coloring book blue bottle gluten-free pinterest artisan,
+            photo booth gochujang raw denim roof party wayfarers mumblecore ugh
+            synth seitan. Subway tile kale chips paleo pug hammock church-key.
+            Master cleanse copper mug normcore taiyaki. Etsy normcore bitters
+            taiyaki craft beer four loko kale chips enamel pin. Tote bag vinyl
+            tousled, pug chicharrones twee disrupt. Celiac flannel deep v
+            try-hard green juice.
           </p>
+
           <p>
-            Landing on web development was an unlikely choice. I hadn’t ever
-            done coding before, and I thought that I had to be superhuman smart
-            to do it. I also didn’t look or act the part. I wasn’t into video
-            games. I didn’t stay up to date on the latest technology. I prided
-            myself on being a flip phone user for longer than everyone else (so
-            hipster{" "}
-            <span role="img" aria-label="wide smiley face">
-              😃
-            </span>
-            ).
+            Chia quinoa raclette, snackwave kickstarter deep v yuccie
+            vexillologist. Cronut prism organic, microdosing heirloom shoreditch
+            +1 skateboard mlkshk. Kogi XOXO knausgaard occupy, polaroid hammock
+            cold-pressed enamel pin narwhal tattooed celiac. Gluten-free pok pok
+            cronut lo-fi. Keytar wolf swag everyday carry squid edison bulb,
+            photo booth try-hard.
           </p>
+
           <p>
-            Technology was vital to me because I used it, but I wasn’t obsessed.
-            So, to be honest, the only reason Web Development was even on the
-            list was because of a friend who happens to be a software developer
-            told me that I’d make a good developer. I was like, “me?.....Nah”.
-            But he was serious. He said that teachers make great developers and
-            that there are these programs called Bootcamps, and I should look
-            into them. I then asked him like a million questions and did some
-            research and realized that I had many misconceptions about web
-            development, programming, and software development.
-          </p>
-          <p>
-            I realized I don’t have to be superhuman smart, that the field is
-            becoming more diverse and accessible and that it’s also not people
-            in dark cubicles hiding away from everyone else. It’s fun,
-            interactive, full of good people, and, on top of that, there’s a
-            thriving and healthy community surrounding it. So I started a
-            Javascript course to see if I could hack it, and I’ll be honest that
-            I struggled. I didn’t get it at first, but I kept going because I
-            enjoyed it. I enjoyed it a lot more than I thought I would. I found
-            myself wanting to understand everything and wanting to figure things
-            out and couldn’t let things go until I got them.
-          </p>
-          <p>
-            I leaned heavily on my fiance, who is a hardware engineer. He helped
-            me understand the basics and encouraged me to keep going. I’m lucky
-            to have him, and everyone needs a champion. But I think I still
-            would have pursued this regardless. Still, I am so glad he has been
-            there.
-          </p>{" "}
-          After completing my Javascript course, I got pretty serious about
-          looking into Bootcamps until I finally landed on Lambda School. I
-          wanted to choose the right one for me, and Lambda School checked off
-          all the boxes. They offer an ISA making it even more possible for me
-          to make this career transition. I enrolled and went through the
-          program.{" "}
-          <p>
-            I had ups and downs, but I kept persevering because I wanted to. In
-            the past, I persevered because I had to. This time around, I
-            persevered because I was enjoying what I was doing and felt
-            inspired.
-            <span role="img" aria-label="dancing woman">
-              💃{" "}
-            </span>
-            I also felt like I still had energy and emotion left for my partner
-            and family after the day ended, which is very different from before.
-            After work most days in my teaching job, I’d be utterly exhausted
-            and had nothing left. I’d even cry just out of exhaustion. Now I
-            stay up late because I just want that page to look good. It’s fun,
-            enjoyable and there’s so much room to grow and learn that I’ll never
-            get bored.
+            Cold-pressed chillwave brooklyn organic wayfarers swag. Tbh kombucha
+            activated charcoal iceland pickled irony kinfolk mlkshk sartorial
+            portland copper mug. La croix helvetica small batch chia squid
+            shoreditch authentic wolf woke dreamcatcher food truck flexitarian
+            normcore butcher. Sriracha +1 air plant occupy, kickstarter yuccie
+            franzen normcore. Church-key 3 wolf moon viral everyday carry
+            truffaut art party.
           </p>
           <hr></hr>
         </div>
